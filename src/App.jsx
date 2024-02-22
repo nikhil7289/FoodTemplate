be84ddb67ee905568e42893af7d95d7104d54083
Nikhil Patil <patilnikhil1033@gmail.com>
2024-02-23 00:42:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from './Components/Carousel';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple Image Carousel</h1>
+      <Carousel/>
+    </div>
+  );
+};
+
+export default App;
